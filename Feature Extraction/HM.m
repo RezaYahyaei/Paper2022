72f1ab870763f5008818baa37ec411dfafde772c
Reza Yahyaei <60220418+RezaYahyaei@users.noreply.github.com>
2022-03-20 12:57:51 +0300

    Fix HM.m
    Another similar file was incorrectly uploaded as HM.m, which was fixed.

--- Feature Extraction/HM.m
+++ Feature Extraction/HM.m
@@ -1,33 +1,49 @@
 % Feature extraction script for HM
 
-kmax = 16; % k_max parameter
-eo = zeros(109, 6, 64, kmax);
-ec = zeros(109, 6, 64, kmax);
+kmax = 6; % k_max parameter
+EO = zeros(109, 6, 64);
+EC = zeros(109, 6, 64);
 for S_i = 1:109
     
     % Eyes Open
+    logk = log(1:kmax);
+    logL = zeros(6, 64, kmax);
     for k = 1:kmax 
         l = zeros(6, 64, k);
         for m = 1:k
             s = floor((1600-m)/k);
             l(:, :, m) = squeeze(sum(abs(diff( ...
                                  PREPROCDAT.EO(S_i, :, :, m:k:m+s*k), ...
-                                 1, 4)), 4)/(s*k*k));
+                                 1, 4)), 4)*(1600-1)/(s*k*k));
         end
-        eo(S_i, :, :, k) = mean(l, 3);
+        logL(:, :, k) = log(mean(l, 3));
+    end
+    for s_i = 1:6
+        for e_i = 1:64
+            a = polyfit(logk, squeeze(logL(s_i, e_i, :))', 1);
+            EO(S_i, s_i, e_i) = -a(1);
+        end
     end
     
     % Eyes Close
+    logL = zeros(6, 64, kmax);
     for k = 1:kmax 
         l = zeros(6, 64, k);
         for m = 1:k
             s = floor((1600-m)/k);
             l(:, :, m) = squeeze(sum(abs(diff( ...
                                  PREPROCDAT.EC(S_i, :, :, m:k:m+s*k), ...
-                                 1, 4)), 4)/(s*k*k));
+                                 1, 4)), 4)*(1600-1)/(s*k*k));
         end
-        ec(S_i, :, :, k) = mean(l, 3);
+        logL(:, :, k) = log(mean(l, 3));
+    end
+    for s_i = 1:6
+        for e_i = 1:64
+            a = polyfit(logk, squeeze(logL(s_i, e_i, :))', 1);
+            EC(S_i, s_i, e_i) = -a(1);
+        end
     end
     
+    disp(['Subject ' num2str(S_i) ' processed.'])
 end
 save HM EO EC
